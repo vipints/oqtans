114ad5fc53a7cc41f9247e87683a2d99d1444ab5
Vipin <vipin at cbio.mskcc.org>
2013-09-19 17:16:43 -0400

    config files added

--- oqtans_tools/DEXSeq/bin/dexseq_config.sh
+++ oqtans_tools/DEXSeq/bin/dexseq_config.sh
@@ -1,10 +1,6 @@
 #!/bin/bash 
 export DEXSEQ_VERSION=1.6.0
-export DEXSEQ_PATH=
-export DEXSEQ_SRC_PATH=${DEXSEQ_PATH}/src
-export DEXSEQ_BIN_PATH=${DEXSEQ_PATH}/bin
-export SAMTOOLS_DIR=/home/vipin/apps/gx-tools/samtools
+export SAMTOOLS_DIR=
 export PYTHON_PATH=/usr/bin/python
+export PYTHONPATH=
 export R_PATH=
-export ENVIRONMENT=galaxy
-
