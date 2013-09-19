cf9020a9ae6fbddd8835dce71043ab349f16c384
Vipin <vipin at cbio.mskcc.org>
2013-09-19 00:09:01 -0400

    config path are updated

---
+++ oqtans_tools/DEXSeq/bin/dexseq_config.sh
@@ -0,0 +1,10 @@
+#!/bin/bash 
+export DEXSEQ_VERSION=1.6.0
+export DEXSEQ_PATH=
+export DEXSEQ_SRC_PATH=${DEXSEQ_PATH}/src
+export DEXSEQ_BIN_PATH=${DEXSEQ_PATH}/bin
+export SAMTOOLS_DIR=/home/vipin/apps/gx-tools/samtools
+export PYTHON_PATH=/usr/bin/python
+export R_PATH=
+export ENVIRONMENT=galaxy
+
