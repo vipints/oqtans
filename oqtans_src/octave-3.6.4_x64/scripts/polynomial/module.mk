84d38788ea25fb60511bbf4b186006dc18596dcb
Vipin <vipin at cbio.mskcc.org>
2013-09-20 18:05:23 -0400

    x64 precompiled version of octave

---
+++ oqtans_src/octave-3.6.4_x64/scripts/polynomial/module.mk
@@ -0,0 +1,33 @@
+FCN_FILE_DIRS += polynomial
+
+polynomial_FCN_FILES = \
+  polynomial/compan.m \
+  polynomial/conv.m \
+  polynomial/deconv.m \
+  polynomial/mkpp.m \
+  polynomial/mpoles.m \
+  polynomial/pchip.m \
+  polynomial/poly.m \
+  polynomial/polyaffine.m \
+  polynomial/polyder.m \
+  polynomial/polyfit.m \
+  polynomial/polygcd.m \
+  polynomial/polyint.m \
+  polynomial/polyout.m \
+  polynomial/polyreduce.m \
+  polynomial/polyval.m \
+  polynomial/polyvalm.m \
+  polynomial/ppval.m \
+  polynomial/ppder.m \
+  polynomial/ppint.m \
+  polynomial/ppjumps.m \
+  polynomial/residue.m \
+  polynomial/roots.m \
+  polynomial/spline.m \
+  polynomial/unmkpp.m
+
+FCN_FILES += $(polynomial_FCN_FILES)
+
+PKG_ADD_FILES += polynomial/PKG_ADD
+
+DIRSTAMP_FILES += polynomial/$(octave_dirstamp)
