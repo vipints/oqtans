304d411151da43e94b3a0b7f981bc349241b742c
Vipin <vipin at cbio.mskcc.org>
2013-09-27 19:39:32 -0400

    resolving the dependencies from global path

--- oqtans_tools/DEXSeq/bin/dexseq_config.sh
+++ oqtans_tools/DEXSeq/bin/dexseq_config.sh
@@ -1,6 +1,6 @@
 #!/bin/bash 
 export DEXSEQ_VERSION=1.6.0
-export SAMTOOLS_DIR=
-export PYTHON_PATH=
+export SAMTOOLS_DIR=${OQTANS_DEP_PATH}/bin/
+export PYTHON_PATH=${OQTANS_PYTHON}
 export PYTHONPATH=
 export R_PATH=
