c2a801db2f8ee87e5de2cc57b66eb6d5b3939041
Vipin <vipin at cbio.mskcc.org>
2013-10-04 18:53:58 -0400

    master config file updated with few other program paths

--- oqtans_config.sh
+++ oqtans_config.sh
@@ -4,7 +4,7 @@
 ##
 
 #
-# Base PATH 
+# Base PATH, Please adapt to your ecosystem  
 # 
 export OQTANS_VERSION=0.1
 export OQTANS_ROOT_PATH=/mnt/oqtans
@@ -12,9 +12,11 @@ export OQTANS_PATH=$OQTANS_ROOT_PATH/oqtans_tools
 export OQTANS_SRC_PATH=${OQTANS_ROOT_PATH}/oqtans_src
 export OQTANS_DEP_PATH=$OQTANS_ROOT_PATH/oqtans_dep
 export OQTANS_PYTHON_VERSION=2.7
+export OQTANS_PYTHON=/usr/bin/python 
+export OQTANS_R=/usr/bin/R 
 
 #
-# TMP directory 
+# From here onwards the default path settings will work 
 #
 export OQTANS_TMP_PATH=$OQTANS_ROOT_PATH/tmp
 mkdir -p $OQTANS_TMP_PATH
@@ -27,13 +29,11 @@ export PYTHONPATH=${OQTANS_DEP_PATH}/lib/python${OQTANS_PYTHON_VERSION}/site-pac
 #
 # Shared library files 
 #
-export LD_LIBRARY_PATH=$OQTANS_DEP_PATH/lib:$OQTANS_DEP_PATH/lib64:$LD_LIBRARY_PATH
+export OQTANS_OCTAVE_VERSION=3.6.4
+export LD_LIBRARY_PATH=$OQTANS_DEP_PATH/lib:$OQTANS_DEP_PATH/lib64:i$OQTANS_DEP_PATH/octave/lib/octave/${OQTANS_OCTAVE_VERSION}:$LD_LIBRARY_PATH
 
 #
 # Shared compiled program PATH 
 # 
-export PATH=$OQTANS_DEP_PATH/bin:$OQTANS_DEP_PATH/octave/bin:$PATH
+export PATH=$OQTANS_DEP_PATH/bin:$OQTANS_DEP_PATH/octave/bin:$OQTANS_PATH/Bowtie/1.0.0/:$OQTANS_PATH/Bowtie/2.1.0/:$OQTANS_PATH/BWA/0.6.2/:$OQTANS_PATH/PALMapper/0.5/:$OQTANS_PATH/Cufflinks/2.1.1/:$OQTANS_PATH/TopHat/1.4.1/:$OQTANS_PATH/TopHat/2.0.9/$PATH
 
-#
-# R module and package management  
-#
