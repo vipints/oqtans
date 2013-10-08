d32334a8d3116adfc460aa827cf6cbaa91f6f05e
Vipin <vipin at cbio.mskcc.org>
2013-10-08 19:10:24 -0400

    General details and automated installation script

---
+++ oqtans_tools/rDiff/0.3/oqtans_make.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#
+# oqtans sub module compile program 
+# 
+
+set -e 
+
+source ./../../../oqtans_config.sh
+cd $OQTANS_PATH/rDiff/0.3/mex
+
+if [ "$1" == "" -o "$1" == "all" ];
+then
+    make octave
+    cd ./../src/locfit/Source
+    make octave
+fi
+
+if [ "$1" == "clean" ];
+then
+    make clean
+    cd ./../src/locfit/Source
+    make clean
+fi
