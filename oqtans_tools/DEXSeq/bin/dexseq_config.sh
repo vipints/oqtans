12d85f412d5a4e54da896bd13c9a4d8a1fcd2fb4
Vipin <vipin at cbio.mskcc.org>
2013-10-08 15:00:55 -0400

    minimal configuration file for tools

--- oqtans_tools/DEXSeq/bin/dexseq_config.sh
+++ oqtans_tools/DEXSeq/bin/dexseq_config.sh
@@ -2,5 +2,5 @@
 export DEXSEQ_VERSION=1.6.0
 export SAMTOOLS_DIR=${OQTANS_DEP_PATH}/bin/
 export PYTHON_PATH=${OQTANS_PYTHON}
-export PYTHONPATH=
+export PYTHONPATH=${OQTANS_DEP_PATH}/lib/site-package:${OQTANS_DEP_PATH}/lib64/site-package:$PYTHONPATH
 export R_PATH=
