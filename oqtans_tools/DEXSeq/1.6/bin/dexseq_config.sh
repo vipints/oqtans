f4eb8bfb71b1e9fbad49c9850f08486278330577
Vipin <vipin at cbio.mskcc.org>
2013-10-08 17:08:46 -0400

    adapt to global path settings

--- oqtans_tools/DEXSeq/1.6/bin/dexseq_config.sh
+++ oqtans_tools/DEXSeq/1.6/bin/dexseq_config.sh
@@ -2,5 +2,5 @@
 export DEXSEQ_VERSION=1.6.0
 export SAMTOOLS_DIR=${OQTANS_DEP_PATH}/bin/
 export PYTHON_PATH=${OQTANS_PYTHON}
-export PYTHONPATH=${OQTANS_DEP_PATH}/lib/site-package:${OQTANS_DEP_PATH}/lib64/site-package:$PYTHONPATH
-export R_PATH=
+export PYTHONPATH=${OQTANS_DEP_PATH}/lib/python${OQTANS_PYTHON_VERSION}/site-package:${OQTANS_DEP_PATH}/lib64/python${OQTANS_PYTHON_VERSION}/site-package:$PYTHONPATH
+export R_PATH=${OQTANS_R}
