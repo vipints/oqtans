1687aad02a7c97b8061e61e36f9e2af86b57fcd8
Vipin <vipin at cbio.mskcc.org>
2013-10-09 20:10:51 -0400

    automated compiled script

---
+++ oqtans_tools/oqtans_make.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#
+# oqtans submodule compile script
+# 
+set -e
+
+basedir=`dirname $0`
+if [ "$basedir" == "." ];
+then
+    basedir=`pwd`/
+fi
+
+echo $basedir
+
+for i in PALMapper/0.5 DESeq/1.12 DESeq2/1.0.19 rDiff/0.3 rQuant/2.2 mTIM/0.2 Trinity/r2013_08_14 BWA/0.6.2 edgeR/0.2
+do
+    cd $basedir/$i
+    echo ==============================================================
+    echo ===================== making $i ===============
+    echo ==============================================================
+    ./oqtans_make.sh $1
+done
